66d1a55ac6702991fcbb6426073011cd7b7c49ab
rxlabz <dev@rxlabz.com>
2016-10-31 15:04:56 +0100

    exemple ajax0

---
+++ ajax/api_blog.php
@@ -0,0 +1,20 @@
+<?php
+
+$articles=[
+  [
+      "title"=>"my first article",
+      "content"=>"my first article content"
+  ],
+  [
+      "title"=>"my 2nd article",
+      "content"=>"my 2nd article content"
+  ],
+  [
+      "title"=>"my 3th article",
+      "content"=>"my 3th article content"
+  ],
+];
+
+echo json_encode($articles);
+
+?>
---
+++ ajax/test.php
@@ -0,0 +1,6 @@
+<?php
+$nom = isset($_GET['nom']) ? $_GET['nom'] : 'world';
+
+echo "coucou $nom";
+
+?>
